295f084c281cfee22d3ec7c4dfa4d875422fae64
priyansh902 <pk953666@gmail.com>
2025-08-31 22:30:36 +0530

    project Setup

---
+++ rural/src/main/java/com/attendence/rural/Sih12Application.java
@@ -0,0 +1,13 @@
+package com.attendence.rural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sih12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sih12Application.class, args);
+	}
+
+}
---
+++ rural/src/test/java/com/attendence/rural/Sih12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendence.rural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sih12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
